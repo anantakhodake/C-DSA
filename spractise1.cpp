f9dec768bb725328809bf32099767c7e9181cc29
ananta khodake <ananta.khodake02@gmail.com>
2024-08-05 12:17:18 +0530

    commit

---
+++ spractise1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n){
+
+    for(int i = 0 ;i<n-1;i++){
+        int min = i;
+        for(int j =i+1 ; j<n ; j++){
+            if(arr[j]<arr[min]){
+                min =j;
+            }
+        }
+        int temp = arr[min];
+        arr[min]= arr[i];
+        arr[i] = temp;
+    }
+      for(int i = 0; i<n; i++){
+        cout << arr[i]<< " ";
+    }
+}
+
+int main (){
+    int arr[] = {10,30,56,87,65};
+    int n = sizeof(arr);
+    selection_sort(arr,n);
+    return 0;
+
+}
