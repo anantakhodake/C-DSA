945e60649e38e54a0654d4a29c93fcc2938d7588
ananta khodake <ananta.khodake02@gmail.com>
2024-08-05 12:00:19 +0530

    succesfully run

--- ssort.cpp
+++ ssort.cpp
@@ -2,24 +2,22 @@
  using namespace std;
 
   void selection_sort(int arr[] , int n){
-    for(int i = 0 ;i<n-1;i++){
+        for(int i = 0 ;i<n;i++)
+        {
+          int min = i;
+          for(int j = i+1 ;j<n;j++) 
+        {
 
-        int min = i ;
-        for(int j = i+1 ;j<n-1;j++){
-
-            if (arr[j]< arr[min]){
+            if (arr[j]< arr[min])
+            {
                 min = j;
             }
         }
         // swapping of array indexes
+           int temp = arr[min];
+           arr[min]=arr[i];
+           arr[i] = temp ;
 
-        int temp = arr[min];
-        arr[min]=arr[i];
-        arr[i] = temp ;
-
-    }
-      for(int i = 0 ; i<n;i++){
-        cout  << arr[i] <<" ";
     }
  }
 
@@ -27,11 +25,15 @@
     int n ;
     cin >> n;
     int arr[n];
-    for(int i = 0 ; i<n-1;i++){
+    for(int i = 0; i<n ;i++)
+    {
         cin >> arr[i];
-    }
-    
-    cout << "\n" ;
+    }  
     selection_sort(arr,n);
+       for(int i = 0 ; i<n; i++)
+       {
+        cout << arr[i] <<" ";
+       }
+    return 0;
  }
 
